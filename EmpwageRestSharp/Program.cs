aed494372a24b80d1d3ecf0211c6dd4dacdf1d59
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-17 21:32:25 +0530

    EmpwageUsingRestSharp

---
+++ EmpwageRestSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpwageRestSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee program using restsharp");
+        }
+    }
+}
